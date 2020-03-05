63408a9797dfb39c3785b37965733fe932f74368
TH <h01581@haaga-helia.fi>
2020-03-05 20:52:37 +0200

    Hide deleted users from UI

--- GetUsers/index.js
+++ GetUsers/index.js
@@ -8,7 +8,7 @@ module.exports = async function (context, req) {
     let response = await fetch(`https://slack.com/api/users.list?token=${SLACK_TOKEN}`);
     let json = await response.json();
 
-    let users = json.members.filter(user => !user.is_bot).map(user => {
+    let users = json.members.filter(user => !user.is_bot && !user.deleted).map(user => {
         return {
             id: user.id,
             name: user.name,
